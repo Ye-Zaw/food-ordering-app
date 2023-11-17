c511307f7fd51b6d03f2f59d1f0688337c544904
yezaw <yezawhtutmwcdev@gmail.com>
2023-11-17 16:15:24 +0630

    first commit

---
+++ src/components/layout/HomeMenu.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function HomeMenu() {
+    return(
+        <section className="">
+            <div className="absolute h-full left-0 right-0 w-full justify-start">
+                <div className="h-48 w-48 absolute -left-12 text-left">
+                    <Image src={'/sallad1.png'} layout={'fill'} objectFit={'contain'} alt={'sallad'} />
+                </div>
+                <div className="h-48 absolute -top-12 right-0 -z-10">
+                    <Image src={'/sallad2.png'} width={107} height={195} alt={'sallad'} />
+                </div>
+            </div>
+            <div className="text-center">
+                <h3 className="uppercase text-gray-600 font-semibold leading-4">
+                    Check out
+                </h3>
+                <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+            </div>
+        </section>
+    )
+}
